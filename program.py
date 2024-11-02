ec31a711b4fd96ae13618820194c8b4cba0989a1
Gabriel Zacarias <gazacharias2002@gmail.com>
2024-11-01 21:52:22 -0300

    identacao

--- program.py
+++ program.py
@@ -28,12 +28,12 @@ matriz_centro = {
 array_ordem = []
 
 matriz_to_kociemba = {
-    "F":[],
-    "R":[],
-    "L":[],
-    "U":[],
-    "B":[],
-    "D":[]
+    "F": [],
+    "R": [],
+    "L": [],
+    "U": [],
+    "B": [],
+    "D": []
 }
 
 cor_to_lado = {
@@ -66,9 +66,9 @@ dicionario_movimentos = {
     "B2": "db_imagens/B2.png"
 }
 
-
 ordem_faces = ['U', 'R', 'F', 'D', 'L', 'B']
 cubo = ""
+
 
 def make_cube():
     cubo = ""
@@ -77,6 +77,7 @@ def make_cube():
             cubo += ''.join(linha)
     return cubo
 
+
 def transpoe_matriz(matriz):
     for cor, valores in matriz.items():
         novo_cor_to_lado = {array_ordem[i]: valor for i, valor in enumerate(cor_to_lado.values())}
@@ -84,6 +85,7 @@ def transpoe_matriz(matriz):
         matriz_to_kociemba[lado].extend(
             [[novo_cor_to_lado[valor] for valor in sublista] for sublista in valores[0]]
         )
+
 
 # Contador e armazenamento das verificações para estabilidade
 ultima_matriz_verificada = None
@@ -94,24 +96,26 @@ ultima_cor_centro = None  # Armazena a última cor de centro salva
 intervalo_verificacao = 2
 tempo_anterior = time.time()
 
+
 # Converter RGB para o nome da cor
 def convert_rgb_to_names(rgb_tuple):
     names = list(color_dict.keys())
     rgb_values = list(color_dict.values())
-    
+
     kdt_db = KDTree(rgb_values)
     distance, index = kdt_db.query([rgb_tuple])
-    
+
     return names[index[0][0]]
+
 
 # Função para desenhar o grid de quadrados fechados no centro da imagem
 def desenhar_grid_quadrado(img, linhas=3, colunas=3, proporcao=0.6):
     height, width, _ = img.shape
     lado_grid = int(min(height, width) * proporcao)
-    
+
     start_y = (height - lado_grid) // 2
     start_x = (width - lado_grid) // 2
-    
+
     tamanho_linha = lado_grid // linhas
     tamanho_coluna = lado_grid // colunas
 
@@ -125,6 +129,7 @@ def desenhar_grid_quadrado(img, linhas=3, colunas=3, proporcao=0.6):
             cv2.rectangle(img, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
 
     return img, start_x, start_y, tamanho_linha, tamanho_coluna
+
 
 # Inicializa a webcam
 cap = cv2.VideoCapture(0)
@@ -140,17 +145,18 @@ nova_matriz_cores = []
 
 while True:
     ret, frame = cap.read()
-    
+
     if not ret:
         break
 
     # Desenhar o grid de quadrados no centro do frame
-    frame_com_grid, start_x, start_y, tamanho_linha, tamanho_coluna = desenhar_grid_quadrado(frame.copy(), linhas, colunas, proporcao)
+    frame_com_grid, start_x, start_y, tamanho_linha, tamanho_coluna = desenhar_grid_quadrado(frame.copy(), linhas,
+                                                                                             colunas, proporcao)
 
     # Atualizar a matriz de cores a cada intervalo de tempo
     if time.time() - tempo_anterior >= intervalo_verificacao:
         tempo_anterior = time.time()
-        
+
         # Converter para o espaço RGB
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         nova_matriz_cores = []
@@ -163,7 +169,7 @@ while True:
                 y_end = start_y + i * tamanho_linha + int(tamanho_linha * (1 + tamanho_reconhecimento) / 2)
                 x_start = start_x + j * tamanho_coluna + int(tamanho_coluna * (1 - tamanho_reconhecimento) / 2)
                 x_end = start_x + j * tamanho_coluna + int(tamanho_coluna * (1 + tamanho_reconhecimento) / 2)
-                
+
                 # Extrair a seção da imagem para reconhecimento
                 hsv_section = frame[y_start:y_end, x_start:x_end]
                 rgb_section = cv2.cvtColor(hsv_section, cv2.COLOR_BGR2RGB)
@@ -200,7 +206,7 @@ while True:
         # Se a matriz for constante por 3 verificações, salvar na matriz de centro correspondente
         if contador_estabilidade >= 3:
             matriz_to_save = matriz_centro[cor_centro_atual]
-            if(len(matriz_to_save) > 0):
+            if (len(matriz_to_save) > 0):
                 print(f'Matriz da cor {cor_centro_atual} já foi salva anteriormente')
                 print('Posicione o cubo em um centro ainda não salvo')
                 print(matriz_centro)
@@ -223,15 +229,15 @@ while True:
                 cor_nome = nova_matriz_cores[i][j]
                 x_start = start_x + j * tamanho_coluna
                 y_start = start_y + i * tamanho_linha
-                cv2.putText(frame_com_grid, cor_nome, (x_start + 10, y_start + 30), 
+                cv2.putText(frame_com_grid, cor_nome, (x_start + 10, y_start + 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
-    if(all(len(lista) > 0 for lista in matriz_centro.values())):
+    if (all(len(lista) > 0 for lista in matriz_centro.values())):
 
         transpoe_matriz(matriz_centro)
         print(matriz_to_kociemba)
         cubo = make_cube()
-        #print(cubo)
+        # print(cubo)
         solve = kociemba.solve(cubo)
         movimentos = solve.split()
 
@@ -250,13 +256,11 @@ while True:
         with open("resolucao_cubo.html", "w") as file:
             file.write(pagina_html)
 
-        print("Página HTML gerada com sucesso: resolucao_cubo.html") 
+        print("Página HTML gerada com sucesso: resolucao_cubo.html")
         webbrowser.open("file://home/kaua/Área de trabalho/WS-VsCode/trabalhoOpenCV/resolucao_cubo.html")
 
         break
 
-
-    
     # Exibir o frame com o grid de quadrados e nomes das cores
     cv2.imshow('Webcam - Reconhecimento de Cores', frame_com_grid)
 
